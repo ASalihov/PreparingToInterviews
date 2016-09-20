ea731fad923fcb0a323eedb5a94497e2f2df805a
ASalihov <a.b.salikhov@gmail.com>
2016-09-20 23:35:21 +0500

    enums

---
+++ PreparingToInterviews/Enums/EnumsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreparingToInterviews
+{
+    public class EnumsService
+    {
+        public EnumsService()
+        {
+            DisplaySomeStuff();
+        }
+
+        private void DisplaySomeStuff()
+        {
+            var en = (short)TestEnum.one;
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(TestEnum)));
+        }
+
+
+    }
+}
